f14fbe32dd462b9d20dae5088023913d8ccc7878
LAPTOP-KTP5ITMU\1 <1442669237@qq.com>
2022-08-25 16:42:03 +0800

    qiankun-base推送到远端

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import { registerMicroApps, start } from 'qiankun'
+
+registerMicroApps([
+  /* name: 微应用的名称，微应用之间必须确保唯一
+  entry: 表示微应用的访问地址
+  container: 微应用挂在到哪个容器下
+  activeRule: 微应用的激活规则 */
+  {
+    name: 'vueApp',
+    entry: '//localhost:8091',
+    container: '#subContainer', // 子应用挂在id为subContainer的div下
+    activeRule: '/app-vue'
+  }
+])
+// 启动 qiankun
+start()
+
 
 new Vue({
   router,
